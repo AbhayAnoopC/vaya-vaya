88a22a7842d1bf7309d6ce329bffedff5dc258f3
Paurav H Param <pauravhp2003@gmail.com>
2024-06-01 02:49:04 -0700

    Add live location tracking

--- Map.jsx
+++ Map.jsx
@@ -1,77 +1,89 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
+import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 const Map = () => {
-  const [mapRegion, setMapRegion] = useState({
-    latitude: 44.131,
-    longitude: -111.1111,
-    latitudeDelta: 0.09133,
-    longitudeDelta: 0.0422,
-  });
-  const [errorMsg, setErrorMsg] = useState(null);
+	const [mapRegion, setMapRegion] = useState({
+		latitude: 44.131,
+		longitude: -111.1111,
+		latitudeDelta: 0.09133,
+		longitudeDelta: 0.0422,
+	});
+	const [errorMsg, setErrorMsg] = useState(null);
 
-  const userLocation = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('Permission denied.');
-      return;
-    }
+	const userLocation = async () => {
+		let { status } = await Location.requestForegroundPermissionsAsync();
+		if (status !== "granted") {
+			setErrorMsg("Permission denied.");
+			return;
+		}
 
-    let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
-    setMapRegion({
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude,
-      latitudeDelta: 0.09133,
-      longitudeDelta: 0.0422,
-    });
-  };
+		let location = await Location.getCurrentPositionAsync({
+			enableHighAccuracy: true,
+		});
+		setMapRegion({
+			latitude: location.coords.latitude,
+			longitude: location.coords.longitude,
+			latitudeDelta: 0.09133,
+			longitudeDelta: 0.0422,
+		});
+	};
 
-  useEffect(() => {
-    userLocation();
+	useEffect(() => {
+		userLocation();
 
-    const locationWatcher = Location.watchPositionAsync(
-      { accuracy: Location.Accuracy.High, timeInterval: 5000, distanceInterval: 10 },
-      (location) => {
-        setMapRegion({
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.09133,
-          longitudeDelta: 0.0422,
-        });
-      }
-    );
+		const locationWatcher = Location.watchPositionAsync(
+			{
+				accuracy: Location.Accuracy.High,
+				timeInterval: 1000,
+				distanceInterval: 100,
+			},
+			(location) => {
+				setMapRegion({
+					latitude: location.coords.latitude,
+					longitude: location.coords.longitude,
+					latitudeDelta: 0.09133,
+					longitudeDelta: 0.0422,
+				});
+				console.log(
+					"Long: ",
+					location.coords.longitude,
+					"Latti : ",
+					location.coords.latitude
+				);
+			}
+		);
 
-    return () => {
-      locationWatcher.then((watcher) => watcher.remove());
-    };
-  }, []);
+		return () => {
+			locationWatcher.then((watcher) => watcher.remove());
+		};
+	}, []);
 
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map} region={mapRegion}>
-        <Marker coordinate={mapRegion} title="Marker" />
-      </MapView>
-      {errorMsg ? <Text>{errorMsg}</Text> : null}
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<MapView style={styles.map} region={mapRegion}>
+				<Marker coordinate={mapRegion} title="Marker" />
+			</MapView>
+			{errorMsg ? <Text>{errorMsg}</Text> : null}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: 'grey',
-  },
-  contentContainer: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  map: {
-    width: '100%',
-    height: '78%',
-  },
+	container: {
+		flex: 1,
+		padding: 24,
+		backgroundColor: "grey",
+	},
+	contentContainer: {
+		flex: 1,
+		alignItems: "center",
+	},
+	map: {
+		width: "100%",
+		height: "78%",
+	},
 });
 
 export default Map;
