97389815aa4fadd43fa17c94511e33e29dde247a
AbhayAnoopC <abhayc@uvic.ca>
2024-06-01 04:03:59 -0700

    added map css

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, { useCallback, useRef } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, SafeAreaView } from 'react-native';
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MapView from 'react-native-maps';
@@ -9,24 +9,27 @@ import Swipe from './Swipe.js'
 const App = () => {
 
   return (
-    <Swipe/>
+    <Swipe />
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
     backgroundColor: 'grey',
+    borderRadius: 20,
+    overflow: 'hidden',
   },
   contentContainer: {
     flex: 1,
+    padding: 24,
     alignItems: 'center',
   },
   map: {
     width: '100%',
-    height: '60%',
-  }
+    aspectRatio: 16 / 9,
+    borderRadius: 20,
+  },
 });
 
 export default App;
--- Swipe.js
+++ Swipe.js
@@ -1,6 +1,6 @@
 
 import React, { useCallback, useRef } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, SafeAreaView } from 'react-native';
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MapView from 'react-native-maps';
@@ -15,34 +15,35 @@ const Swipe = () => {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      
+
       <View style={styles.container}>
-        {/* <Button title="do someting" /> */}
-        <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
+        <MapView style={styles.map} />
         <BottomSheet
           ref={bottomSheetRef}
           onChange={handleSheetChanges}
           snapPoints={['40%', '90%']}
-          //overDragResistanceFactor = {10}
-          opacity = {1.9}
+          opacity={1.9}
         >
           <BottomSheetView style={styles.contentContainer}>
             <Text>Awesome 🎉</Text>
           </BottomSheetView>
         </BottomSheet>
       </View>
+
     </GestureHandlerRootView>
+
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    padding: 5,
     backgroundColor: 'grey',
+    width: '100%',
+    // borderRadius: '20'
   },
+
   contentContainer: {
     flex: 1,
     alignItems: 'center',
@@ -50,6 +51,8 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '60%',
-  }})
 
-  export default Swipe
+  }
+})
+
+export default Swipe
